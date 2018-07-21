a70b19d02fe8afc3aeb17d41be07aa6f9612ebb7
Savio Fernandes <saviocarlosjd@gmail.com>
2018-07-21 15:39:58 -0300

    colocando o projeto no github

---
+++ EMG interface/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace EMG_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(109, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 433);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "stop";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(257, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "EMG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EMG interface/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace EMG_interface
+{
+    public partial class Form1 : Form
+    {
+        Thread ThreadAquisicao;
+        Thread ThreadPlotagem;
+        Mutex Mutexbuffer;
+        Mutex MutexPlotagem;
+        string []sinal = new string[1000];
+        int contador = 0;
+        string valorlido;
+        Boolean rw;
+        Boolean plot;
+
+        public void Aquisicao()
+        {
+            while (true)
+            {
+                Mutexbuffer.WaitOne();
+
+                if (rw == true)
+                {
+                    sinal[contador] = serialPort1.ReadLine();
+                    //sinal[contador] = contador;
+                    rw = false;
+                }
+
+                Mutexbuffer.ReleaseMutex();
+                Thread.Sleep(200);
+            }
+        }
+
+        public void Plotagem()
+        {
+            while (true)
+            {
+                Mutexbuffer.WaitOne();
+                if (rw == false)
+                {
+                    if (contador < 1000)
+                    {
+                        valorlido = sinal[contador];
+                        contador++;
+                    }
+                    else if(contador >= 1000)
+                    {
+                        contador = 0;
+                    }
+                    
+                    rw = true;
+                    Mutexbuffer.ReleaseMutex();
+                    Thread.Sleep(200);
+
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen == false)
+            serialPort1.Open();
+            ThreadAquisicao = new Thread(Aquisicao);
+            ThreadPlotagem = new Thread(Plotagem);
+            Mutexbuffer = new Mutex();
+            MutexPlotagem = new Mutex();
+            plot = true;
+            if (ThreadAquisicao.IsAlive != true)
+            {
+                ThreadAquisicao.Start();
+                ThreadPlotagem.Start();
+                timer1.Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (ThreadAquisicao.IsAlive == true)
+            {
+                ThreadAquisicao.Abort();
+                ThreadPlotagem.Abort();
+                plot = false;  
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MutexPlotagem.WaitOne();
+            if (plot != false && valorlido != null)
+            {
+                listBox1.Items.Add( valorlido );
+            }
+            MutexPlotagem.ReleaseMutex();
+            timer1.Stop();
+            timer1.Start();
+        }
+    }
+}
